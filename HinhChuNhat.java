2b8cb0be8010f5ba06f6b5f64b8715b5eabdc0e6
Quannisme <121941593+Quannisme@users.noreply.github.com>
2023-05-11 12:24:46 +0700

    Add files via upload

---
+++ HinhChuNhat.java
@@ -0,0 +1,10 @@
+package hinh;
+import java.util.*;
+public class HinhChuNhat {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Nhap canh a");
+	int a;
+	a=sc.nextInt();
+}
+}
