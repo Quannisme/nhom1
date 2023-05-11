4b759dd54ccb057521e9a340d7122c3a36dd2adf
minhai1110 <133186064+minhai1110@users.noreply.github.com>
2023-05-11 13:05:24 +0700

    hải sửa
    hải sửa

---
+++ HinhChuNhat (1).java
@@ -0,0 +1,16 @@
+package hinh;
+import java.util.*;
+public class HinhChuNhat {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Nhap canh a");
+	int a;
+	a=sc.nextInt();
+	// dang xuan son : thieu canh b
+	System.out.println("Nhap canh b :");
+	int b;
+	b=sc.nextIn();
+	// Hoang Minh Hai: Thieu canh c
+	System.out.println("Nhap canh c:");
+}
+}
